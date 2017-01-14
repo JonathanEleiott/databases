c07f94f1e2d7991a4a077b8fda34b27234e6143f
JonathanEleiott <jonathan.eleiott@gmail.com>
2017-01-13 23:00:43 -0800

    Add client and set db,models, and controller

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,65 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.dbConnection.connect();
+
+      db.dbConnection.end();
+    }, // a function which produces all the messages
+
+    post: function () {
+      db.dbConnection.connect();
+
+      db.dbConnection.end();
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      db.dbConnection.connect();
+
+      db.dbConnection.end();
+    },
+
+    post: function (userObj) {
+      // db.dbConnection();
+      db.dbConnection.connect();
+
+      var queryString = 'SELECT * FROM messages';
+      db.dbConnection.query(queryString, function(err, rows, fields) {
+        if (err) {
+          console.log('error is ', err);
+        }
+
+        for (var i in rows) {
+          console.log('Post Titles: ', rows[i].post_title);
+        }
+      });
+
+      db.dbConnection.end();
+    }
   }
 };
 
+//console.log goes to nodemon
+//node mySQL query
+//insert into set ?
+
+/*
+
+  connection.connect();
+   
+  var queryString = 'SELECT * FROM wp_posts';
+   
+  connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+   
+      for (var i in rows) {
+          console.log('Post Titles: ', rows[i].post_title);
+      }
+  });
+   
+  connection.end();
+
+*/
